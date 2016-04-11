9c5ef4e24316ccf7769d4d71909cafa0e2bc84dc
Greg Baker <ggbaker@sfu.ca>
2016-04-11 13:56:55 -0700

    working two-way communication

--- slides/static/react-logic.js
+++ slides/static/react-logic.js
@@ -2,6 +2,8 @@ var aspect = 4/3;
 var pathDecimals = 2;
 var widthUnits = 100;
 var heightUnits = 100/aspect;
+var pollInterval = 2000;
+var fontVH = 1;
 
 var stroke_attrs = function() {
     return {
@@ -12,23 +14,33 @@ var stroke_attrs = function() {
     };
 };
 
-var svg_attrs = function() {
+var size_props = function () {
+    var style = {}
+    if ( window.innerWidth/window.innerHeight > aspect ) {
+        // window wider than aspect ratio
+        style.width = (100*aspect) + 'vh';
+        style.height = '100vh';
+        style.fontSize = fontVH + 'vh';
+    } else {
+        // window higher than aspect ratio
+        style.width = '100vw';
+        style.height =  (100 / aspect) + 'vw';
+        style.fontSize = (fontVH / aspect) + 'vw';
+    }
+    return style;
+};
+
+var svg_attrs = function () {
     var slideSizeCSS;
+    var sizeStyle = size_props();
     var style = {
         zIndex: 10,
         position: 'absolute',
         top: 0,
         left: 0,
+        width: sizeStyle.width,
+        height: sizeStyle.height,
     };
-    if ( window.innerWidth/window.innerHeight > aspect ) {
-        // window wider than aspect ratio
-        style.width = (100*aspect) + 'vh';
-        style.height = '100vh';
-    } else {
-        // window higher than aspect ratio
-        style.width = '100vw';
-        style.height =  (100 / aspect) + 'vw';
-    }
 
     return {
         id: 'slide-overlay',
@@ -39,16 +51,21 @@ var svg_attrs = function() {
     }
 };
 
+var roundCoordinate = function (x) {
+    var factor = Math.pow(10,pathDecimals);
+    return Math.round(x*factor)/factor
+};
+
 var pathAnnotationToPath = function (ann, id, i) {
     var n = ann.pointsX.length;
     var d;
     var key = id + '-' + i
 
     if ( n > 1 ) {
-        d = 'M' + ann.pointsX[0].toFixed(pathDecimals) + ',' + ann.pointsY[0].toFixed(pathDecimals);
+        d = 'M' + ann.pointsX[0] + ',' + ann.pointsY[0];
         d += ' L'
         for ( var i=1; i<n; i++ ) {
-            d += ' ' + ann.pointsX[i].toFixed(pathDecimals) + ',' + ann.pointsY[i].toFixed(pathDecimals);
+            d += ' ' + ann.pointsX[i] + ',' + ann.pointsY[i];
         }
         return <path key={key} {...stroke_attrs()} d={d} />
     }
@@ -80,8 +97,8 @@ var AnnotationSet = React.createClass({
             order: 9999 + this.state.numAdded,
             data: {elements: [{
                 elt: 'path',
-                pointsX: [x],
-                pointsY: [y],
+                pointsX: [roundCoordinate(x)],
+                pointsY: [roundCoordinate(y)],
             }]},
         }
 
@@ -99,19 +116,44 @@ var AnnotationSet = React.createClass({
         var y = y / this.props.body.offsetHeight * heightUnits;
 
         var workingElement = this.state.workingElement;
-        workingElement.data.elements[0].pointsX = workingElement.data.elements[0].pointsX.concat([x])
-        workingElement.data.elements[0].pointsY = workingElement.data.elements[0].pointsY.concat([y])
+        var path = workingElement.data.elements[0];
+        path.pointsX = path.pointsX.concat([roundCoordinate(x)])
+        path.pointsY = path.pointsY.concat([roundCoordinate(y)])
 
-        this.setState(function(oldState, props) {
-            return {
+        this.setState({
                 workingElement: workingElement,
-            }
-        });
+            });
 
     },
     pathEnd: function() {
         this.setState({
             paint: false,
+        });
+
+        var workingElement = this.state.workingElement;
+        var postData = {'data': workingElement.data};
+        $.ajax({
+            url: annotation_api_url,
+            type: 'POST',
+            dataType: 'json',
+            contentType:"application/json; charset=utf-8",
+
+            beforeSend: function (request)
+            {
+                request.setRequestHeader("X-CSRFToken", csrf_token);
+            },
+            data: JSON.stringify(postData),
+
+            success: function(data) {
+                var ann = this.state.data.slice(0, this.state.previousLength);
+                ann.push(data);
+                this.setState({
+                    data: ann
+                });
+            }.bind(this),
+            error: function(xhr, status, err) {
+                console.error(this.props, status, err.toString());
+            }.bind(this)
         });
     },
 
@@ -133,7 +175,7 @@ var AnnotationSet = React.createClass({
         if ( this.state.paint ) {
             e.preventDefault();
             this.pathMore(e.changedTouches[0].pageX, e.changedTouches[0].pageY);
-        }
+        }className
     },
     handleDrawStop: function(e) {
         if ( this.state.paint ) {
@@ -166,6 +208,7 @@ var AnnotationSet = React.createClass({
     },
     componentDidMount: function() {
         this.loadFromServer();
+        setInterval(this.loadFromServer, pollInterval);
     },
     render: function() {
         var annotations = this.state.data.map(function (a) {
@@ -180,8 +223,8 @@ var AnnotationSet = React.createClass({
             onMouseLeave: this.handleDrawStop,
             onTouchStart: this.handleTouchStart,
             onTouchMove: this.handleTouchMove,
-            onMouseUp: this.handleDrawStop,
-            onMouseCancel: this.handleDrawStop,
+            onTouchEnd: this.handleDrawStop,
+            onTouchCancel: this.handleDrawStop,
         };
 
         return (
@@ -215,7 +258,15 @@ var Slide = React.createClass({
         this.loadFromServer();
     },
     render: function() {
+        var html = document.getElementsByTagName('html').item(0);
         var body = document.getElementsByTagName('body').item(0);
+        var sizeStyle = size_props();
+        body.className = 'slide';
+        body.style.width = sizeStyle.width;
+        body.style.height = sizeStyle.height;
+        // set font-size on <html> so 1rem is a predictable 1% of slide height.
+        html.style.fontSize = sizeStyle.fontSize;
+
         return (
             <div id="slide">
             <div id="slide-contents" dangerouslySetInnerHTML={this.rawMarkup()}></div>
